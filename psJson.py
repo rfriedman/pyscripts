f93224c38f81778c5cfc8b0163833396c3328942
rfriedman <friedman.richard@gmail.com>
2017-12-18 08:19:12 -0500

    add output to dictionary class

--- psJson.py
+++ psJson.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
 
 import sys
+#class to create dictionary object from output headers and process output columns
+class outputToDictionary(object):
+    def __init__(self,value):
+        self.lines=value
+    def makeHeaders(self):
+        self.headers = self.lines[:1].split()
+
 
 if __name__ == "__main__":
     strLine = sys.stdin.readlines()
     for line in strLine:
-        print line.splitlines()
+        strSplit = line.split()
+        for a in strSplit:
+           print a
         sys.stderr.write("DEBUG: got line: " + line)
-        sys.stdout.write(line)
+       # sys.stdout.write(line)
