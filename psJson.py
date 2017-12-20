570f3dd872a4f3d47c335743abf1d026419fc77c
rfriedman <friedman.richard@gmail.com>
2017-12-20 00:10:55 -0500

    serialized ouput to json from ps process

--- psJson.py
+++ psJson.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python
 
 import sys
+import json
 #class to create dictionary object from output headers and process output columns
 class outputToDictionary(object):
     def __init__(self,value):
         self.lines=value
-    def makeHeaders(self):
-        self.headers = self.lines[:1].split()
-
+        self.stringList()
+        self.serialtojson()
+    def stringList(self):
+        self.headers = self.lines[0].split()
+        self.headercnt=len(self.headers)
+    def serialtojson(self):
+        self.data={}
+        self.data['process']=[]
+        for line in self.lines[1:]:
+            self.values=line.split()
+            self.data['process'].append({  
+          self.headers[0]:self.values[0],
+          self.headers[1]:self.values[1],
+          self.headers[2]:self.values[2],
+          self.headers[3]:self.values[3],
+          })
+    def outputjson(self):
+        with open('ps_data.txt', 'w') as outfile:  
+           json.dump(self.data, outfile, indent=4)
 
 if __name__ == "__main__":
     strLine = sys.stdin.readlines()
-    for line in strLine:
-        strSplit = line.split()
-        for a in strSplit:
-           print a
-        sys.stderr.write("DEBUG: got line: " + line)
+    p = outputToDictionary(strLine)
+    p.outputjson()
+
+
        # sys.stdout.write(line)
