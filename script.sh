c775b67724f5863c43396a39d7238fc19cc017de
rfriedman <friedman.richard@gmail.com>
2018-01-02 00:39:09 -0500

    scripts

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-ssh richard@192.168.0.6 'ssh-keygen'
-cat /home/pi/.ssh/id_rsa | ssh richard@192.168.0.6 'cat>>/home/richard/.ssh/authorized_keys_replace'
-ssh richard@192.168.0.6 'cat /home/richard/.ssh/id_rsa.pub'>/home/pi/.ssh/authorized_keys_replace
-ssh richard@192.168.0.6 'ssh-keygen'
-cat /home/pi/.ssh/id_rsa | ssh richard@192.168.0.6 'cat>>/home/richard/.ssh/authorized_keys_replace'
-ssh richard@192.168.0.6 'cat /home/richard/.ssh/id_rsa.pub'>/home/pi/.ssh/authorized_keys_replace
+ssh pi@192.168.0.7 'ssh-keygen'
+cat /home/pi/.ssh/id_rsa.pub | ssh pi@192.168.0.7 'cat>>/home/pi/.ssh/authorized_keys'
+ssh pi@192.168.0.7 'cat /home/pi/.ssh/id_rsa.pub'>>/home/pi/.ssh/authorized_keys_replace
+cat /home/pi/jsonlog.sh | ssh pi@192.168.0.7 'cat>>/home/pi/jsonlog.sh && sudo cp /home/pi/jsonlog.sh /usr/local/sbin'
