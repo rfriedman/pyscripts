c724dc39b0a767a113a0f0265dba3aa44e8bfca2
rfriedman <friedman.richard@gmail.com>
2018-01-05 21:03:37 +0000

    test batch files

--- jsonlog.sh
+++ jsonlog.sh
@@ -3,6 +3,6 @@ hostname -I > /tmp/out.txt &&
 date >> /tmp/out.txt && 
 cat /sys/class/net/wlan0/address >> /tmp/out.txt &&
  ps -a >> /tmp/out.txt && 
- cat /tmp/out.txt | /usr/bin/ssh pi@192.168.0.7 '/home/pi/psjson.py 2>>error.log'  
+ cat /tmp/out.txt | /usr/bin/ssh pi@172.16.24.179 '/home/pi/psjson.py 2>>error.log'  
  
  #rm /tmp/out.txt
--- script.sh
+++ script.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
-ssh pi@172.16.24.46 'mkdir /home/pi/.ssh'
 ssh pi@172.16.24.47 'mkdir /home/pi/.ssh'
-ssh pi@172.16.24.46 'ssh-keygen'
 ssh pi@172.16.24.47 'ssh-keygen'
-cat /home/pi/.ssh/key.host | ssh pi@172.16.24.46 'cat>>/home/pi/.ssh/known_hosts'
 cat /home/pi/.ssh/key.host | ssh pi@172.16.24.47 'cat>>/home/pi/.ssh/known_hosts'
-cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.46 'cat>>/home/pi/.ssh/authorized_keys'
 cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.47 'cat>>/home/pi/.ssh/authorized_keys'
-ssh pi@172.16.24.46 'cat /home/pi/.ssh/id_rsa.pub'>>/home/pi/.ssh/authorized_keys
-ssh pi@172.16.24.47 'cat /home/pi/.ssh/id_rsa.pub'>>/home/pi/.ssh/authorized_keys
-cat /home/pi/jsonlog.sh | ssh pi@172.16.24.46 'cat>/home/pi/jsonlog.sh && sudo cp /home/pi/jsonlog.sh /usr/local/sbin/jsonlog.sh&&rm /home/pi/jsonlog.sh'
+ssh pi@172.16.24.47 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
 cat /home/pi/jsonlog.sh | ssh pi@172.16.24.47 'cat>/home/pi/jsonlog.sh && sudo cp /home/pi/jsonlog.sh /usr/local/sbin/jsonlog.sh&&rm /home/pi/jsonlog.sh'
-cat /home/pi/crontab | ssh pi@172.16.24.46 'cat>/home/pi/crontab && sudo cp /home/pi/crontab /etc/crontab&&rm /home/pi/crontab'
 cat /home/pi/crontab | ssh pi@172.16.24.47 'cat>/home/pi/crontab && sudo cp /home/pi/crontab /etc/crontab&&rm /home/pi/crontab'
-ssh pi@172.16.24.46 'sudo chmod 755 /usr/local/sbin/jsonlog.sh'
 ssh pi@172.16.24.47 'sudo chmod 755 /usr/local/sbin/jsonlog.sh'
