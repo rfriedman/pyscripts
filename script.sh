bc695f123bf662770ebaefe0d26238b10d445c42
rfriedman <friedman.richard@gmail.com>
2018-01-05 13:34:10 -0500

    test files

--- script.sh
+++ script.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-ssh richard@192.168.0.7 'mkdir /home/richard/.ssh'
-ssh richard@192.168.0.7 'ssh-keygen'
-cat /home/pi/.ssh/key.host | ssh richard@192.168.0.7 'cat>>/home/richard/.ssh/known_hosts'
-cat /home/pi/.ssh/id_rsa.pub | ssh richard@192.168.0.7 'cat>>/home/richard/.ssh/authorized_keys'
-ssh richard@192.168.0.7 'cat /home/richard/.ssh/id_rsa.pub'>>/home/pi/.ssh/authorized_keys
-cat /home/pi/jsonlog.sh | ssh richard@192.168.0.7 'cat>/home/richard/jsonlog.sh && sudo cp /home/richard/jsonlog.sh /usr/local/sbin/jsonlog.sh&&rm /home/richard/jsonlog.sh'
-cat /home/pi/crontab | ssh richard@192.168.0.7 'cat>/home/richard/crontab && sudo cp /home/richard/crontab /etc/crontab&&rm /home/richard/crontab'
-ssh richard@192.168.0.7 'sudo chmod 755 /usr/local/sbin/jsonlog.sh'
+ssh pi@172.16.24.46 'mkdir /home/pi/.ssh'
+ssh pi@172.16.24.47 'mkdir /home/pi/.ssh'
+ssh pi@172.16.24.46 'ssh-keygen'
+ssh pi@172.16.24.47 'ssh-keygen'
+cat /home/pi/.ssh/key.host | ssh pi@172.16.24.46 'cat>>/home/pi/.ssh/known_hosts'
+cat /home/pi/.ssh/key.host | ssh pi@172.16.24.47 'cat>>/home/pi/.ssh/known_hosts'
+cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.46 'cat>>/home/pi/.ssh/authorized_keys'
+cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.47 'cat>>/home/pi/.ssh/authorized_keys'
+ssh pi@172.16.24.46 'cat /home/pi/.ssh/id_rsa.pub'>>/home/pi/.ssh/authorized_keys
+ssh pi@172.16.24.47 'cat /home/pi/.ssh/id_rsa.pub'>>/home/pi/.ssh/authorized_keys
+cat /home/pi/jsonlog.sh | ssh pi@172.16.24.46 'cat>/home/pi/jsonlog.sh && sudo cp /home/pi/jsonlog.sh /usr/local/sbin/jsonlog.sh&&rm /home/pi/jsonlog.sh'
+cat /home/pi/jsonlog.sh | ssh pi@172.16.24.47 'cat>/home/pi/jsonlog.sh && sudo cp /home/pi/jsonlog.sh /usr/local/sbin/jsonlog.sh&&rm /home/pi/jsonlog.sh'
+cat /home/pi/crontab | ssh pi@172.16.24.46 'cat>/home/pi/crontab && sudo cp /home/pi/crontab /etc/crontab&&rm /home/pi/crontab'
+cat /home/pi/crontab | ssh pi@172.16.24.47 'cat>/home/pi/crontab && sudo cp /home/pi/crontab /etc/crontab&&rm /home/pi/crontab'
+ssh pi@172.16.24.46 'sudo chmod 755 /usr/local/sbin/jsonlog.sh'
+ssh pi@172.16.24.47 'sudo chmod 755 /usr/local/sbin/jsonlog.sh'
