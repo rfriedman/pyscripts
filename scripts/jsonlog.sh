622447ae6bedfe93cb0b3761a791ff9c1863064d
richard friedman <friedman.richard@gmail.com>
2018-01-15 01:07:49 +0000

    config

--- scripts/jsonlog.sh
+++ scripts/jsonlog.sh
@@ -2,7 +2,10 @@
 hostname -I > /tmp/out.txt &&
 date >> /tmp/out.txt && 
 cat /sys/class/net/wlan0/address >> /tmp/out.txt &&
- ps -a >> /tmp/out.txt && 
- cat /tmp/out.txt | /usr/bin/ssh pi@172.16.24.179 '/home/pi/psjson.py 2>>error.log'  
+ ps -a >> /tmp/out.txt  
+cat /tmp/out.txt | /home/pi/psjson &
+
+
+# cat /tmp/out.txt | /usr/bin/ssh pi@172.16.24.179 '/home/pi/psjson.py 2>>error.log' &  
  
  #rm /tmp/out.txt
