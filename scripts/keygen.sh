92dbda0acf69d6de68efc93948283d4e2174aa3a
rfriedman <friedman.richard@gmail.com>
2018-04-09 08:15:53 -0400

    added key-scan to keygen.json

--- scripts/keygen.sh
+++ scripts/keygen.sh
@@ -1,43 +1,46 @@
 #!/bin/sh
-ssh pi@172.16.24.3 'mkdir /home/pi/.ssh'
-ssh pi@172.16.24.4 'mkdir /home/pi/.ssh'
-ssh pi@172.16.24.6 'mkdir /home/pi/.ssh'
-ssh pi@172.16.24.9 'mkdir /home/pi/.ssh'
-ssh pi@172.16.24.10 'mkdir /home/pi/.ssh'
-ssh pi@172.16.24.11 'mkdir /home/pi/.ssh'
-ssh pi@172.16.24.12 'mkdir /home/pi/.ssh'
-ssh pi@172.16.24.13 'mkdir /home/pi/.ssh'
-ssh pi@172.16.24.3 'ssh-keygen'
-ssh pi@172.16.24.4 'ssh-keygen'
-ssh pi@172.16.24.6 'ssh-keygen'
-ssh pi@172.16.24.9 'ssh-keygen'
-ssh pi@172.16.24.10 'ssh-keygen'
-ssh pi@172.16.24.11 'ssh-keygen'
-ssh pi@172.16.24.12 'ssh-keygen'
-ssh pi@172.16.24.13 'ssh-keygen'
-ssh pi@172.16.24.3 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
-ssh pi@172.16.24.4 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
-ssh pi@172.16.24.6 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
-ssh pi@172.16.24.9 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
-ssh pi@172.16.24.10 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
-ssh pi@172.16.24.11 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
-ssh pi@172.16.24.12 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
-ssh pi@172.16.24.13 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
-cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.2 'cat>>/home/pi/.ssh/authorized_keys'
-cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.3 'cat>>/home/pi/.ssh/authorized_keys'
-cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.4 'cat>>/home/pi/.ssh/authorized_keys'
-cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.6 'cat>>/home/pi/.ssh/authorized_keys'
-cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.9 'cat>>/home/pi/.ssh/authorized_keys'
-cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.10 'cat>>/home/pi/.ssh/authorized_keys'
-cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.11 'cat>>/home/pi/.ssh/authorized_keys'
-cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.12 'cat>>/home/pi/.ssh/authorized_keys'
-cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.13 'cat>>/home/pi/.ssh/authorized_keys'
-ssh pi@172.16.24.2 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
-ssh pi@172.16.24.3 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
-ssh pi@172.16.24.4 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
-ssh pi@172.16.24.6 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
-ssh pi@172.16.24.9 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
-ssh pi@172.16.24.10 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
-ssh pi@172.16.24.11 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
-ssh pi@172.16.24.12 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
-ssh pi@172.16.24.13 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
+ssh pi@172.16.24.26 'mkdir /home/pi/.ssh'
+ssh pi@172.16.24.28 'mkdir /home/pi/.ssh'
+ssh pi@172.16.24.29 'mkdir /home/pi/.ssh'
+ssh pi@172.16.24.31 'mkdir /home/pi/.ssh'
+ssh pi@172.16.24.32 'mkdir /home/pi/.ssh'
+ssh pi@172.16.24.36 'mkdir /home/pi/.ssh'
+ssh pi@172.16.24.37 'mkdir /home/pi/.ssh'
+ssh pi@172.16.24.38 'mkdir /home/pi/.ssh'
+ssh pi@172.16.24.39 'mkdir /home/pi/.ssh'
+ssh pi@172.16.24.26 'ssh-keygen'
+ssh pi@172.16.24.28 'ssh-keygen'
+ssh pi@172.16.24.29 'ssh-keygen'
+ssh pi@172.16.24.31 'ssh-keygen'
+ssh pi@172.16.24.32 'ssh-keygen'
+ssh pi@172.16.24.36 'ssh-keygen'
+ssh pi@172.16.24.37 'ssh-keygen'
+ssh pi@172.16.24.38 'ssh-keygen'
+ssh pi@172.16.24.39 'ssh-keygen'
+ssh pi@172.16.24.26 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
+ssh pi@172.16.24.28 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
+ssh pi@172.16.24.29 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
+ssh pi@172.16.24.31 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
+ssh pi@172.16.24.32 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
+ssh pi@172.16.24.36 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
+ssh pi@172.16.24.37 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
+ssh pi@172.16.24.38 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
+ssh pi@172.16.24.39 'ssh-keyscan -H -t rsa 172.16.24.192>>/home/pi/.ssh/known_hosts'
+cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.26 'cat>>/home/pi/.ssh/authorized_keys'
+cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.28 'cat>>/home/pi/.ssh/authorized_keys'
+cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.29 'cat>>/home/pi/.ssh/authorized_keys'
+cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.31 'cat>>/home/pi/.ssh/authorized_keys'
+cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.32 'cat>>/home/pi/.ssh/authorized_keys'
+cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.36 'cat>>/home/pi/.ssh/authorized_keys'
+cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.37 'cat>>/home/pi/.ssh/authorized_keys'
+cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.38 'cat>>/home/pi/.ssh/authorized_keys'
+cat /home/pi/.ssh/id_rsa.pub | ssh pi@172.16.24.39 'cat>>/home/pi/.ssh/authorized_keys'
+ssh pi@172.16.24.26 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
+ssh pi@172.16.24.28 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
+ssh pi@172.16.24.29 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
+ssh pi@172.16.24.31 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
+ssh pi@172.16.24.32 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
+ssh pi@172.16.24.36 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
+ssh pi@172.16.24.37 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
+ssh pi@172.16.24.38 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
+ssh pi@172.16.24.39 'cat /home/pi/.ssh/id_rsa.pub' | cat>>/home/pi/.ssh/authorized_keys
